4f9954d183f063d5cf3f6d98bfef163decb4a732
Fasal321 <>
2022-04-28 14:26:20 +0500

    Added new Screen

---
+++ app/src/main/java/com/example/dependecy_injection/ui/activities/DetailScreen.kt
@@ -0,0 +1,12 @@
+package com.example.dependecy_injection.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dependecy_injection.R
+
+class DetailScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_screen)
+    }
+}
